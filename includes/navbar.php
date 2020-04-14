bcbd96a0c56ef20e95dea953ea407f4cbbf9b4b2
Mat08251 <duguet@simplon-charleville.fr>
2020-04-14 17:26:02 +0200

    navbar-beta

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,9 +8,26 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7d77d119e1.js" crossorigin="anonymous"></script>
     <title>barre d'accueil</title>
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../php/index.php"><img class="logo" src="../images/logo-simplon_23.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+            <li class="nav-item1">
+                <a class="nav-link mr-sm-2" href="#"><span class="icon"><i class="fas fa-home"></i></span>ACCUEIL</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mr-sm-2" href="#"><span class="icon"><i class="fas fa-user"></i></span>SE CONNECTER</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
 </body>
 </html>
