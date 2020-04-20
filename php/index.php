e11a2d3effbb3d5ca1bb8f7dcaecd58a9ae11703
Mat08251 <duguet@simplon-charleville.fr>
2020-04-20 18:43:18 +0200

    index-beta-v2

--- php/index.php
+++ php/index.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/7d77d119e1.js" crossorigin="anonymous"></script>
+    <script src="../parallax/parallax.min.js"></script>
     <title>page d'accueil</title>
 </head>
 <body>
@@ -48,21 +49,93 @@
     </div>
 
     <!--card des formations-->
-    <div class="card mb-3" style="max-width: 1040px;">
+    <div class="formation card " >
         <div class="row no-gutters">
             <div class="col-md-4">
             <img src="..." class="card-img" alt="...">
             </div>
-            <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+            <div class="cadre col-md-8">
+                <div class="card-body">
+                    <div class="gauche">
+                    <h5 class="card-title">DEVELOPPEUR WEB</h5>
+                    <p class="card-text">Début d'inscription : 1/1/2020<br>
+                                        Fin d'inscription : 2/1/2020</p>
+                    <p class="card-text">Début de la formation : 5/1/2020</p>
+                    <p class="card-text">Durée de la formation : 6mois</p>
+                    <button type="button" class="btn  btn-lg" style="background-color: #F01616">En savoir plus >></button>
+                    </div>                    
+                </div>
+                <div class="card-body">
+                    <div class="droite">
+                    <p class="card-text">La création d’un compte est obligatoire pour<br>
+                                         pretendre a l’inscription de la formation </p>
+                    <button type="button" class="btn  btn-lg" style="background-color: #F01616">INSCRIPTION >></button>
+                    </div>                    
+                </div>
             </div>
         </div>
     </div>
 
+    <div class="parallax" data-parallax="scroll" data-z-index="" data-image-src="../images/team-work-business-startup 1.png"></div>
+
+
+    <!--card des formations-->
+    <div class="formation card  justify-center" >
+        <div class="row no-gutters">
+            <div class="col-md-4">
+            <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="cadre col-md-8">
+                <div class="card-body">
+                    <div class="gauche">
+                    <h5 class="card-title">DEVELOPPEUR WEB</h5>
+                    <p class="card-text">Début d'inscription : 1/1/2020<br>
+                                        Fin d'inscription : 2/1/2020</p>
+                    <p class="card-text">Début de la formation : 5/1/2020</p>
+                    <p class="card-text">Durée de la formation : 6mois</p>
+                    <button type="button" class="btn  btn-lg" style="background-color: #F01616">En savoir plus >></button>
+                    </div>                    
+                </div>
+                <div class="card-body">
+                    <div class="droite">
+                    <p class="card-text">La création d’un compte est obligatoire pour<br>
+                                         pretendre a l’inscription de la formation </p>
+                    <button type="button" class="btn  btn-lg" style="background-color: #F01616">INSCRIPTION >></button>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="parallax" data-parallax="scroll" data-z-index="" data-image-src="../images/team-work-business-startup 1.png"></div>
+
+    <!--card des formations-->
+    <div class="formation card  justify-center" >
+        <div class="row no-gutters">
+            <div class="col-md-4">
+            <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="cadre col-md-8">
+                <div class="card-body">
+                    <div class="gauche">
+                    <h5 class="card-title">DEVELOPPEUR WEB</h5>
+                    <p class="card-text">Début d'inscription : 1/1/2020<br>
+                                        Fin d'inscription : 2/1/2020</p>
+                    <p class="card-text">Début de la formation : 5/1/2020</p>
+                    <p class="card-text">Durée de la formation : 6mois</p>
+                    <button type="button" class="btn  btn-lg" style="background-color: #F01616">En savoir plus >></button>
+                    </div>                    
+                </div>
+                <div class="card-body">
+                    <div class="droite">
+                    <p class="card-text">La création d’un compte est obligatoire pour<br>
+                                         pretendre a l’inscription de la formation </p>
+                    <button type="button" class="btn  btn-lg" style="background-color: #F01616">INSCRIPTION >></button>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include ('../includes/footer.php'); ?>
 </body>
 </html>
