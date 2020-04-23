979d7b41f6567dedd55d0537c7354df909f26f6d
Mat08251 <duguet@simplon-charleville.fr>
2020-04-23 19:06:28 +0200

    page-formulaire-alpha

---
+++ includes/entete-formulaire.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../css/entete-formulaire.css">
+<div class="titre-formulaire text-center">
+       <div class="col-md-12">
+          <h1 class="Titre-entete">Développeur(se) Web & web mobile</h1>
+       </div>
+</div>
