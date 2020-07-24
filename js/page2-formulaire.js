c5d76d904d6e90ba2e4131a51bbb20643db5d75b
Mat08251 <duguet@simplon-charleville.fr>
2020-07-24 14:47:35 +0200

    animation page 3 formulaire

--- js/page2-formulaire.js
+++ js/page2-formulaire.js
@@ -1,6 +1,44 @@
 const sr=ScrollReveal();
 
+sr.reveal('.div_formulaire', {
+    scale: 0.5,
+    duration: 2500
+});
+
 sr.reveal('.etape2', {
     origin: 'left',
-    distance: '50px'
+    distance: '150px',
+    duration: 2500,
+    delay: 400
+});
+
+sr.reveal('.cadre1', {
+    scale: 0.8,
+    interval: 500,
+    duration: 3000
+});
+
+sr.reveal('.cadre2', {
+    scale: 0.8,
+    interval: 500,
+    duration: 3000
+});
+
+sr.reveal('.cadre3', {
+    scale: 0.8,
+    interval: 500,
+    duration: 3000
+});
+
+sr.reveal('.cadre4', {
+    scale: 0.8,
+    interval: 500,
+    duration: 3000
+});
+
+sr.reveal('.bouton', {
+    origin: 'bottom',
+    duration: 2800,
+    distance: '50px',
+    delay: 400
 });
